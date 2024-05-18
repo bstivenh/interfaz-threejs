0589ba02f0b8d721fc2cbde32cc181a2f023b212
Stiven Hernandez <hbrayan1216@gmail.com>
2024-05-18 13:47:05 -0500

    🔥 Cambios

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ const geometry35 = new THREE.BoxGeometry(5, 1.3, 0);
 const geometry36 = new THREE.BoxGeometry(1.2, 3.5, 0);
 const geometry37 = new THREE.BoxGeometry(1.2, 2.5, 0);
 const geometry38 = new THREE.BoxGeometry(9.5, 1, 0);
+const geometry39 = new THREE.BoxGeometry(1, 1, 0);
+const geometry40 = new THREE.BoxGeometry(3, 1, 0);
 
 const material = new THREE.MeshBasicMaterial({ color: 0x3D85C6 });
 const material2 = new THREE.MeshBasicMaterial({ color: 0xcccccc });
@@ -65,8 +67,10 @@ const cube35 = new THREE.Mesh(geometry35, material2);
 const cube36 = new THREE.Mesh(geometry36, material2);
 const cube37 = new THREE.Mesh(geometry37, material2);
 const cube38 = new THREE.Mesh(geometry38, material2);
+const cube39 = new THREE.Mesh(geometry39, material2);
+const cube40 = new THREE.Mesh(geometry40, material2);
 
-scene.add(cube, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20, cube21, cube30, cube31, cube32, cube33, cube34, cube35, cube36, cube37, cube38);
+scene.add(cube, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20, cube21, cube30, cube31, cube32, cube33, cube34, cube35, cube36, cube37, cube38, cube39, cube40);
 
 camera.position.z = 5;
 
@@ -140,6 +144,12 @@ cube35.position.x = -2;
 
 cube38.position.y = -0.55;
 cube38.position.x = -1;
+
+cube39.position.y = -1.55;
+cube39.position.x = -4.5;
+
+cube40.position.y = 0;
+cube40.position.x = 0.3;
 
 // caminos Verticales
 cube13.position.y = 2;
