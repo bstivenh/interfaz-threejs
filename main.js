81566bde64d39620ee5a8223d40800a04578654d
Stiven Hernandez <hbrayan1216@gmail.com>
2024-05-18 10:50:54 -0500

    ✨ Nuevo camino

--- main.js
+++ main.js
@@ -5,11 +5,9 @@ scene.background = new THREE.Color(0xcccccc);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 
 const geometry = new THREE.BoxGeometry(0, 10, 1);
 const geometry2 = new THREE.BoxGeometry(13, 1, 0);
@@ -19,16 +17,10 @@ const geometry5 = new THREE.BoxGeometry(1, 3, 0);
 const geometry6 = new THREE.BoxGeometry(3, 1, 0);
 const geometry7 = new THREE.BoxGeometry(1, 4.3, 0);
 const geometry8 = new THREE.BoxGeometry(2, 1, 0);
-
-
-
-
+const geometry9 = new THREE.BoxGeometry(22.3, 1.1, 0);
 
 const material = new THREE.MeshBasicMaterial({ color: 0x3D85C6 });
 const material2 = new THREE.MeshBasicMaterial({ color: 0xcccccc });
-
-
-
 
 const cube = new THREE.Mesh(geometry, material);
 const cube2 = new THREE.Mesh(geometry2, material);
@@ -41,20 +33,13 @@ const cube8 = new THREE.Mesh(geometry2, material);
 const cube9 = new THREE.Mesh(geometry2, material);
 const cube10 = new THREE.Mesh(geometry2, material);
 const cube11 = new THREE.Mesh(geometry2, material);
-
-
-
 const cube12 = new THREE.Mesh(geometry5, material2);
 const cube13 = new THREE.Mesh(geometry6, material2);
 const cube14 = new THREE.Mesh(geometry7, material2);
 const cube15 = new THREE.Mesh(geometry8, material2);
+const cube16 = new THREE.Mesh(geometry9, material2);
 
-
-
-
-
-
-scene.add(cube, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15);
+scene.add(cube, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16);
 
 camera.position.z = 5;
 
@@ -109,6 +94,9 @@ cube14.position.x = 6.9;
 
 cube15.position.y = 0.9;
 cube15.position.x = 6;
+
+cube16.position.y = 3.2;
+cube16.position.x = -6;
 
 function animate() {
     // cube.rotation.x += 0.01;
