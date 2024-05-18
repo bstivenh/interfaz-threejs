f5745270b92e1cbdb7b8fa443c29bf8b62f4c2c9
EdwCami <edwin9608@gmail.com>
2024-05-18 09:45:48 -0500

    modificaciones del laberinto

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ document.body.appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry(0, 10, 1);
 const geometry2 = new THREE.BoxGeometry(13, 1, 0);
 const geometry3 = new THREE.BoxGeometry(13, 1, 0);
-const geometry4 = new THREE.BoxGeometry(1, 9, 0);
+const geometry4 = new THREE.BoxGeometry(1, 10, 0);
 const geometry5 = new THREE.BoxGeometry(1, 9, 0);
 const geometry6 = new THREE.BoxGeometry(1, 9, 0);
 
@@ -32,8 +32,8 @@ const cube = new THREE.Mesh(geometry, material);
 const cube2 = new THREE.Mesh(geometry2, material);
 const cube3 = new THREE.Mesh(geometry3, material);
 const cube4 = new THREE.Mesh(geometry4, material);
-const cube5 = new THREE.Mesh(geometry5, material2);
-const cube6 = new THREE.Mesh(geometry6, material2);
+const cube5 = new THREE.Mesh(geometry2, material);
+const cube6 = new THREE.Mesh(geometry2, material);
 const cube7 = new THREE.Mesh(geometry2, material);
 const cube8 = new THREE.Mesh(geometry2, material);
 const cube9 = new THREE.Mesh(geometry2, material);
@@ -42,33 +42,54 @@ const cube11 = new THREE.Mesh(geometry2, material);
 
 
 
+const cube12 = new THREE.Mesh(geometry5, material2);
+const cube13 = new THREE.Mesh(geometry6, material2);
+
+
+
 
 
 scene.add(cube, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11);
 
 camera.position.z = 5;
+
+// Cubos horinzontales 
 cube.position.y = 1;
 cube.position.x = -6;
-cube2.position.y = 3.5;
-cube2.position.x = 1;
-cube3.position.y = -3.5;
-cube3.position.x = 0;
+
+
 cube4.position.y = -1;
 cube4.position.x = 7;
-cube5.position.y = -3;
-cube5.position.x = -4;
-cube6.position.y = 3;
-cube6.position.x = 6;
-cube7.position.y = 3;
+
+// Cubos verticales
+
+cube2.position.y = 3.5;
+cube2.position.x = 0;
+
+cube3.position.y = -3.5;
+cube3.position.x = 0;
+
+
+cube5.position.y = 2.5;
+cube5.position.x = 0;
+
+cube6.position.y = 1.5;
+cube6.position.x = 0;
+
+cube7.position.y = 1;
 cube7.position.x = 0;
-cube8.position.y = 4;
+
+cube8.position.y = -1;
 cube8.position.x = 0;
-// cube9.position.y = 5;
-// cube9.position.x = 1;
-// cube10.position.y = 5.5;
-// cube10.position.x = 1;
-// cube11.position.y = 6;
-// cube11.position.x = 1;
+
+cube9.position.y = 0;
+cube9.position.x = 1;
+
+cube10.position.y = -2;
+cube10.position.x = 1;
+
+cube11.position.y = -2.5;
+cube11.position.x = 1;
 
 
 function animate() {
